4c6cae04e2b5562aa9e8266a987e7f3c30b51b97
Epilogue Media <epiloguemedia@epilogues-mbp-3.attlocal.net>
2016-11-28 18:09:14 -0800

    CD wip and updated main

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,18 @@
 #include "Parser.h"
 #include "Line.h"
 #include "Command.h"
+#include <unistd.h>
+#include <stdlib.h>
 
-int main() {
-  
+int main() { 
   int status = 0;
-  
+
+  setenv("program_root", getenv("PWD"), 1); 
+  std::cout << "setting program_root to " << getenv("program_root") << std::endl << std::endl; 
+
   do 
   {
-       std::cout << "$ ";
+       std::cout << getenv("PWD") << "$ ";
 
        std::string input;
     
